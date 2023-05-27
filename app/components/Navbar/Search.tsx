dda22afe4394e65ce2171996ada67be2a88717ec
Cecilia Deng <ceciliadeng12@berkeley.edu>
2023-05-27 16:15:40 -0700

    navbar done

--- app/components/Navbar/Search.tsx
+++ app/components/Navbar/Search.tsx
@@ -10,7 +10,7 @@ const Search = () => {
                 </div>
 
                 <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
-                    Any Week
+                    Any Week hi
                 </div>
                 
                 <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
